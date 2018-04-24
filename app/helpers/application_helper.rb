c7b1f649bc759d9336d34b89844fbaf9057a9118
Austin Chappell <austin@cratebind.com>
2018-04-24 11:02:39 -0500

    show articles on user page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def avatar_for(user, options = { height: 100, width: 100 })
+    avatar_url = "https://placeimg.com/#{options[:width]}/#{options[:height]}/people"
+    image_tag(avatar_url, alt: user.username)
+  end
 end
