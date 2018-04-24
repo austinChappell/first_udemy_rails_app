a71c8e99c31f2b192df7a2209817f152527b84ba
Austin Chappell <austin@cratebind.com>
2018-04-24 10:40:05 -0500

    can edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     @users = User.all
   end
@@ -18,7 +20,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User successfully updated"
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
